dba6551f80db4e08dd899031130c50d642eba1d3
kulklex <kulklex@gmail.com>
2023-03-10 07:32:56 +0100

    Created and Impl Navbar and Header Components

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import people from '../assets/people.png'
+import ai from '../assets/ai.png' 
+import Image from 'next/image'
+
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <div id='home' className='flex flex-col md:flex-row'>
+      <div className='flex flex-[4] flex-col justify-center items-start mr-10 lg:mr-20 px-4 lg:px-8 py-8 md:px-24'>
+        <h1 className='gradient-text text-xl lg:text-2xl font-extrabold font-serif'>
+          Let&apos;s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p className='text-base lg:text-lg text-[#81AFDD]'>
+          Yet bed any for traveling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+        </p>
+
+        <div className="w-full my-3 flex flex-row">
+          <input type="email" placeholder=" Your Email Address" className='flex-[2] w-full min-h-[50px] font-medium border-solid border-2 border-[#031B34] bg-[#031B34] outline-none text-[#fff] font-serif'/>
+          <button type="button" className='flex-[0.6] w-full bg-[#FF4820] text-[#fff] min-h-[50px] py-0 px-2 font-normal rounded-md outline-none'>Get Started</button>
+        </div>
+
+        <div className='w-full flex justify-start items-center flex-col md:flex-row mt-6'>
+          <Image src={people} alt='people' className='w-[180px] lg:w-[200px] h-[40px] lg:h-[50px]' />
+          <p className='m-0 ml-4 text-xs lg:text-base text-white align-center'>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+      </div>
+      
+      <div className='flex-[2] flex justify-center items-center'>
+        <Image src={ai} alt="AI" className='w-full h-full' />
+      </div>
+    </div>
+  )
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+import logo from '../assets/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
+
+
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center p-4 lg:py-8 lg:px-24'>
+      <div className='md:flex-[1] flex'>
+        <div className='hidden mr-8 md:flex md:items-center md:justify-start'>
+          <Image src={logo} alt="logo" className='w-16 h-4' />
+        </div>
+        <div className='flex flex-row justify-center'>
+          <p className='text-white font-serif font-medium text-xs md:text-sm lg:text-base capitalize my-0 lg:mx-4 mx-1 cursor-pointer'>
+            <Link href='/'>Home</Link>
+          </p>
+          <p className='text-white font-serif font-medium text-xs md:text-sm lg:text-base capitalize my-0 lg:mx-4 mx-1 cursor-pointer'>
+            <Link href='/whatGPT3'>What is GPT?</Link>
+          </p>
+          <p className='text-white font-serif font-medium text-xs md:text-sm lg:text-base capitalize my-0 lg:mx-4 mx-1 cursor-pointer'>
+            <Link href='/possibility'>Open AI</Link>
+          </p>
+          <p className='text-white font-serif font-medium text-xs md:text-sm lg:text-base capitalize my-0 lg:mx-4 mx-1 cursor-pointer'>
+            <Link href='/features'>Case Studies</Link>
+          </p>
+          <p className='text-white font-serif font-medium text-xs md:text-sm lg:text-base capitalize my-0 lg:mx-4 mx-1 cursor-pointer'>
+            <Link href='/blog'>Library</Link>
+          </p>
+        </div>
+      </div>
+
+      <div className='hidden md:flex justify-end items-center'>
+        <p className='text-white font-serif font-medium text-xs md:text-sm lg:text-base capitalize my-0 mx-1 md:mx-2 lg:mx-4 cursor-pointer'>
+          Sign in
+        </p>
+        <button type="button" className='p-2  text-white bg-[#FF4820] font-serif rounded-md font-medium text-xs md:text-sm lg:text-base border-none outline-none'>
+          Sign up
+        </button>
+      </div>
+
+      <div className='md:hidden'>
+        {toggleMenu 
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (<div className='scale-up-center'>
+          <div className='flex justify-end items-end flex-col text-end right-0 top-10 mt-4 rounded shadow'>
+            <p className='text-white font-serif font-medium text-xs lg:text-base capitalize my-1 lg:mx-4 mx-1 cursor-pointer'>
+              Sign in
+            </p>
+            <button type="button" className='p-2 text-white bg-[#FF4820] font-serif rounded-md font-medium text-xs lg:text-base border-none outline-none'>
+              Sign up
+            </button>
+          </div>
+        </div>)}
+      </div>
+    </div>
   )
 }
